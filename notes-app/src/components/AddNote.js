9977db59f979b165923893cd27743d8b4c236177
jwhprince <piratkaignat@gmail.com>
2022-04-14 01:30:01 +0500

    character count and save space between text in note

--- notes-app/src/components/AddNote.js
+++ notes-app/src/components/AddNote.js
@@ -2,9 +2,13 @@
  
  const AddNote = ({ handleAddNote }) => {
      const [noteText, setNoteText] = useState('');
+     const characterLimit = 200;
        
      const handleChange = (e) => {
-          setNoteText(e.target.value);
+         if(characterLimit - e.target.value.length >= 0){
+
+             setNoteText(e.target.value);
+         }
      };
 
      const handleSaveClick = () => {
@@ -24,7 +28,7 @@
                 onChange={handleChange}
              ></textarea>
              <div className='note-footer'>
-                 <small>200 Remaining</small>
+                 <small>{characterLimit - noteText.length} Remaining</small>
                  <button className='save' onClick={handleSaveClick}>Save</button>
              </div>
          </div>
