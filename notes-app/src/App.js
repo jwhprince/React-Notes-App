b9380eff795fc09502ad33b9f26c508b1dc9d8e3
jwhprince <piratkaignat@gmail.com>
2022-04-14 01:08:02 +0500

    add new notes component

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -25,9 +25,22 @@ const App = () => {
       date: "19/04/2022",
      },
 ]);
+
+const addNote = (text) => {
+  const date = new Date();
+  const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+  }
+ const newNotes = [...notes, newNote];
+ setNotes(newNotes);
+
+};
+
   return (
     <div className="container">
-         <NotesList notes={notes}/>  
+         <NotesList notes={notes} handleAddNote={addNote}/>  
     </div>
   );
 };
