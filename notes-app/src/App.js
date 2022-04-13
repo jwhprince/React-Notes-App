fd11c6e9e06304ca89b6fd72e05e5fd65b86605c
jwhprince <piratkaignat@gmail.com>
2022-04-14 01:55:15 +0500

    delete note

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -38,9 +38,18 @@ const addNote = (text) => {
 
 };
 
+const deleteNote = (id) => {
+  const newNotes = notes.filter((note)=> note.id !== id);
+  setNotes(newNotes);
+}
+
   return (
     <div className="container">
-         <NotesList notes={notes} handleAddNote={addNote}/>  
+         <NotesList 
+         notes={notes} 
+         handleAddNote={addNote}
+         handleDeleteNote={deleteNote}
+         />  
     </div>
   );
 };
